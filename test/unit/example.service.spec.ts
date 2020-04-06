de6205546a1912f97f2204d94e39cf370349626a
Adam Drag <adam.drag@outlook.com>
2020-04-06 07:46:43 +0200

    Fixed unit test

--- test/unit/example.service.spec.ts
+++ test/unit/example.service.spec.ts
@@ -9,11 +9,12 @@ describe('ExampleService tests', ()=> {
     const sandbox:SinonSandbox = sinon.createSandbox();
     it('should return ExampleModel with data read from example client', async() => {
         //given
+        const id = 'XYZ123';
         const exampleModelFromClient:ExampleModel = {data:'some data'};
-        sandbox.stub(ExampleClient.prototype, 'getExampleData').resolves(exampleModelFromClient);
+        sandbox.stub(ExampleClient.prototype, 'getExampleData').withArgs(id).resolves(exampleModelFromClient);
 
         //when
-        const exampleModelFromService:ExampleModel = await new ExampleService().getExampleData();
+        const exampleModelFromService:ExampleModel = await new ExampleService().getExampleData(id);
 
         //then
         expect(exampleModelFromService).to.eql(exampleModelFromClient);
